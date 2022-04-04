c4a890f6085c4c0b2a5821ce2d164bd94a0affe5
marioalbertocer <mario.albceron@gmail.com>
2022-04-04 18:45:52 -0500

    eddited allTaxaGFs_orthofinder.py

--- allTaxaGFs_orthofinder.py
+++ allTaxaGFs_orthofinder.py
@@ -1,1 +1,15 @@
-primera linea
+import os,sys
+
+f = open(sys.argv[1], "r").readlines()
+f = f[1:]
+
+for line_report in f:
+	line_report = line_report.strip()
+	gf = line_report.split("\t")[0]
+	rest = line_report.split("\t")[1:]
+	c = 0
+	for taxon in rest:
+		if int(taxon) != 0:
+			c += 1
+	print("%s,%s" % (gf, str(c)))
+	
